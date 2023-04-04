0a8f4026e69db611467c0d06b06acd46dea6e275
said.T <tarhisaid@gmail.com>
2023-04-04 00:45:36 +0000

    login

--- pages/Login.js
+++ pages/Login.js
@@ -1,11 +1,12 @@
 
 import styles from "../styles/Login.module.css";
-import { GoogleOutlined, FacebookOutlined } from "@ant-design/icons";
+import { GoogleOutlined,  } from "@ant-design/icons";
 import React, { useState } from "react";
 import firebase from "firebase/compat/app";
 import { auth } from "../firebase";
 import { signInWithRedirect } from "firebase/auth";
 import { GoogleAuthProvider } from "firebase/auth";
+import { Button, Card, Grid, Loading, Text } from "@nextui-org/react";
 
 const provider = new GoogleAuthProvider();
 
@@ -26,43 +27,30 @@ const Login = () => {
     }
   };
   
-  const handleFacebookSignIn = async () => {
-    try {
-      setLoading(true);
-      await signInWithRedirect(
-        new firebase.auth.FacebookAuthProvider()
-      );
-    } catch (error) {
-      setError(error.message);
-    } finally {
-      setLoading(false);
-    }
-  };
+ 
   return !loading?(
-    <div id="login-page" className={styles["login-page"]}>
-      <div id="login-card" className={styles["login-card"]}>
-        <h2>Welcome to One Touch</h2>
-        {error && <p className={styles.error}>{error}</p>}
-        <button
-          className={styles["login-button"]}
-          onClick={handleGoogleSignIn}
+    <Grid.Container css={{position:"absolute",backgroundColor:"LimeGreen",height:"100%"}}>
+    <Card >
+     <Card.Header css={{backgroundColor:"SkyBlue"}}>
+        <Text>Welcome to Babel</Text>
+        {error && <Text >{error}</Text>}
+        </Card.Header>
+        <Card.Body css={{backgroundColor:"Gold"}}>
+        <Button
+          
+          onPress={()=>handleGoogleSignIn()}
           disabled={loading}
         >
           <GoogleOutlined /> Sign In with Google
-        </button>
-        <br />
-        <br />
-        <button
-          className={styles["login-button"]}
-          onClick={handleFacebookSignIn}
-          disabled={loading}
-        >
-          <FacebookOutlined /> Sign In with Facebook
-        </button>
-        {loading && <p className={styles.loading}>Loading...</p>}
-      </div>
-    </div>
-  ):(null);
+        </Button>
+        </Card.Body>
+        <Card.Footer css={{backgroundColor:"black"}}>
+        
+        {loading && <Text color="white" >Loading...</Text>}
+        </Card.Footer>
+      </Card>
+      </Grid.Container>
+  ):(<Grid.Container justify="center" css={{position:"absolute",top:"40%"}}><Text>Loading</Text><Loading size="xl"  type="points-opacity"></Loading></Grid.Container>);
 };
 
 export default Login;
