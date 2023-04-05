cced63a250269618378e9b840dba2398734cfaea
said.T <tarhisaid@gmail.com>
2023-04-05 23:57:22 +0000

    BASY PLACE PARKING

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "firebase/firestore";
-import Accueil  from "../components/Accueil";
+
 import Login from "./Login";
 import { auth } from "../firebase";
 
 import { useAuthState } from "react-firebase-hooks/auth";
 import NavBar from "./NavBar";
+import { Grid, Loading, Text } from "@nextui-org/react";
 
 export default function Home() {
   const [user, loading, error] = useAuthState(auth);
-  return (
-    <>
-  {user ? <NavBar /> : <Login />}
-    </> 
-  );
-}
+  const [redirectToLogin, setRedirectToLogin] = useState(false);
 
+  useEffect(() => {
+    if (!user && !loading) {
+      setRedirectToLogin(true);
+      setTimeout(() => {
+        setRedirectToLogin(false);
+      }, 2000); // delay of 2 seconds
+    }
+  }, [user, loading]);
+
+  if (redirectToLogin) {
+    return <Login />;
+  }
+
+  return <>{user ? <NavBar /> : <Grid.Container justify="center" css={{position:"absolute",top:"40%"}}><Text>Loading</Text><Loading size="xl"  type="points-opacity"></Loading></Grid.Container>}</>;
+}
