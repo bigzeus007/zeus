8d019d5d392a5af5c921143c4d9253c41f417bbb
said.T <tarhisaid@gmail.com>
2023-04-05 00:04:44 +0000

    AVATAR AND BADGE

--- components/AddCar.js
+++ components/AddCar.js
@@ -68,7 +68,7 @@ export default function TakePicture() {
         video.play();
         setPlayingVideo(true);
       } catch (err) {
-        console.error(err);
+        C.error(err);
       }
     }
   };
