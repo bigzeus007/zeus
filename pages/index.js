1e47f8cc25325c9eec3abc1f4c0afdd36b4f6d67
said.T <tarhisaid@gmail.com>
2023-04-06 00:29:13 +0000

    CONNECTING

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "firebase/firestore";
 
 import Login from "./Login";
@@ -6,24 +6,13 @@ import { auth } from "../firebase";
 
 import { useAuthState } from "react-firebase-hooks/auth";
 import NavBar from "./NavBar";
-import { Grid, Loading, Text } from "@nextui-org/react";
 
 export default function Home() {
   const [user, loading, error] = useAuthState(auth);
-  const [redirectToLogin, setRedirectToLogin] = useState(false);
-
-  useEffect(() => {
-    if (!user && !loading) {
-      setRedirectToLogin(true);
-      setTimeout(() => {
-        setRedirectToLogin(false);
-      }, 2000); // delay of 2 seconds
-    }
-  }, [user, loading]);
-
-  if (redirectToLogin) {
-    return <Login />;
-  }
-
-  return <>{user ? <NavBar /> : <Grid.Container justify="center" css={{position:"absolute",top:"40%"}}><Text>Loading</Text><Loading size="xl"  type="points-opacity"></Loading></Grid.Container>}</>;
+  return (
+    <>
+  {user ? <NavBar /> : <Login />}
+    </> 
+  );
 }
+
