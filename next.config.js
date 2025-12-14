46f7ea7d25e4bd38d6131e8ab30456f76576bda5
said.T <tarhisaid@gmail.com>
2025-12-14 20:06:22 +0100

    fix: next build (esmExternals + transpilePackages)

--- next.config.js
+++ next.config.js
@@ -1,12 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
-
-module.exports = {
   images: {
-    domains: ['firebasestorage.googleapis.com','lh3.googleusercontent.com'],
+    domains: ["firebasestorage.googleapis.com", "lh3.googleusercontent.com"],
   },
-}
+
+  experimental: {
+    esmExternals: "loose",
+  },
+
+  transpilePackages: [
+    "@ant-design/icons",
+    "@ant-design/icons-svg",
+    "rc-util",
+    "rc-pagination",
+    "rc-picker",
+    "rc-tooltip",
+    "rc-trigger",
+    "rc-align",
+    "rc-motion",
+    "rc-resize-observer",
+  ],
+};
+
+module.exports = nextConfig;
